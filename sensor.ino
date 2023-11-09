7705eff2c683271bc14ac24aa52f80ebaeac5904
Huwuy <102541653+Huwuy@users.noreply.github.com>
2023-11-09 15:51:32 +0700

    Add files via upload

--- sensor.ino
+++ sensor.ino
@@ -1,20 +1,36 @@
-#include <LiquidCrystal_I2C.h>
-
 #include <Wire.h>
 #include <BH1750.h>
 
-BH1750 Sensor;
+BH1750 sensor;
+
+int i = 112;
+int x = 100;
 
 void setup() {
+  Wire.begin();
+  sensor.begin();
   Serial.begin(9600);
-  Sensor.begin();
   Serial.println("BH1750 Test begin");
+  pinMode(11, OUTPUT);
 }
 
 void loop() {
-  float lux = Sensor.readLightLevel();
+  float lux = sensor.readLightLevel();
   Serial.print("Light: ");
   Serial.print(lux);
   Serial.println(" lx");
-  delay(1000); 
+  
+  if (lux < x) {
+    i++;
+  } 
+  else if(lux > x){
+    i--; // Decrease brightness
+  }
+  analogWrite(11,i);
+  if (lux >= x - 3 && lux <= x + 3)
+  {
+    analogWrite(11,i);
+    delay(900000);
+  }
+  delay(100);
 }
